8ba8941366da17304baba2e32aac5abacc9edf43
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-11-26 15:14:42 -0600

    Added search function
    Added new search function. This function takes a value via user input and alerts them if that value is currently in the stack. If the value is on the stack, the user is also told at which position in the stack that value is, with position 0 corresponding to the top of the stack.

--- SingleLinkedListStack.js
+++ SingleLinkedListStack.js
@@ -8,9 +8,10 @@
   1. push a value
   2. pop a value
   3. print the entire stack
-  4. close the program
+  4. search the stack for a specified value
+  5. close the program
 
-+I currently believe I can push values, pop values, and print the stack based on user input
++I currently believe I can push values, pop values, print the stack, and search the stack based on user input
   -pop function feels like it could be improved
 */
 
@@ -93,17 +94,19 @@ function sllStack(){
 
   //print stack function:
   function doPrint(){
+    var position=0; //variable to display each values postion in the stack
     if(top.value!=null){
       //there are things on the stack
       var stackString="Stack contains(in order): \r";  //will hold entirety of stack as single string for simple printing
 
       var tmp=top;
-      stackString=stackString+tmp.value+"\r";  //add value to the string
-
+      stackString=stackString+position+". "+tmp.value+"\r";  //add value to the string
+      position++;
       //traverse queue, top to bottom, adding values to string
       while(tmp!=bottom){
         tmp=tmp.prevNode;
-        stackString=stackString+tmp.value+"\r";  //add value to the string
+        stackString=stackString+position+". "+tmp.value+"\r";  //add value to the string
+        position++;
       }
       alert(stackString); //print the entirety of the stack as single string
     }else{
@@ -112,10 +115,43 @@ function sllStack(){
     }
   }
 
+  //function to search for specified value in stack:
+  function doSearch(searchValue){
+    var position=0; //position of specified value in stack
+
+    if(top.value!=null){
+      //there are things on the stack
+      var tmp=top;
+
+      //traverse stack, starting from top, searching for specific value
+      do{
+        if(tmp.value==searchValue){
+          //value found, exit loop
+          break;
+        }
+        tmp=tmp.prevNode;
+        position++;
+      }while(tmp!=bottom);
+
+      //at this point, we've either found the value or are at the bottom of the stack
+      if(tmp.value==searchValue){
+        //value was found in loop or bottom of stack was specified value
+        alert(searchValue+" found in stack at position: "+position+"\r(position 0 being the next to be popped)");
+      }else{
+        alert(searchValue+" was not found in the queue");
+      }
+
+    }else{
+      //stack is empty
+      alert("stack is empty, cannot search for a value");
+    }
+  }
+
   var publicAPI={
     push:doPush,
     pop:doPop,
-    print:doPrint
+    print:doPrint,
+    search:doSearch
   };
   return publicAPI;
 
@@ -165,7 +201,7 @@ function main(){
   var choice;
   do{
     //keep asking user for input until they choose "terminate program" or hit "cancel"
-    choice=prompt("What would you like to do? \r 1. push a value \r 2. pop a value \r 3. print the stack \r 4. terminate program");
+    choice=prompt("What would you like to do? \r 1. push a value \r 2. pop a value \r 3. print the stack \r 4. search the stack \r 5. terminate program");
     if(choice==1){
       //push a value
       var userInput=prompt("Enter a value to push on to stack");
@@ -179,7 +215,12 @@ function main(){
       //print the satck
       stack.print();
     }
-    else if(choice==4 || choice==null){
+    else if(choice==4){
+      //search for specified value
+      var searchInput=prompt("enter a value to search for");
+      stack.search(searchInput);
+    }
+    else if(choice==5 || choice==null){
       //user wishes to terminate program
       alert("thank you for using this program!");
     }
@@ -187,6 +228,6 @@ function main(){
       //invalid choice
       alert("please enter a valid choice");
     }
-  }while(choice!=4 && choice!=null)
+  }while(choice!=5 && choice!=null)
 }
 main();
